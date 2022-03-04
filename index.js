9ecdbdddbf62eb74163a06e212cc62809fbb0d45
PaulineZeziola <paulinez@wanadoo.fr>
2022-03-04 17:50:17 +0100

    calculator without division

--- index.js
+++ index.js
@@ -11,7 +11,8 @@
  * --> afficher le résultat
  */
 
-// function to listen submit event on form
+/* ================ FORM ================ */
+
  const init = function(){
   let form = document.getElementById('calcul');
   form.addEventListener('submit', getItem);
@@ -22,16 +23,81 @@ const getItem = function(event){
   event.preventDefault();
   let input = document.getElementById('calcul-input');
   let value = input.value;
-  cleanInput();
-  calculator(value);
+  additionCalculator(value);
 }
 
-const cleanInput = function(){
-  let input = document.getElementById('calcul-input');
-  input.value = "";
+/* ================= CALCULATOR ============== */
+
+// split the input value in terms of parenthesis
+const split = function (value, operator) {
+	const result = [];
+	let parenthesis = 0;
+	let splitValue = "";
+	for (let i = 0; i < value.length; ++i) {
+		const partValue = value[i];
+		if (partValue == '(') {
+			parenthesis++;
+		} else if (partValue == ')') {
+			parenthesis--;
+		}
+		if (parenthesis == 0 && operator == partValue) {
+			result.push(splitValue);
+			splitValue = "";
+		} else splitValue += partValue;
+	}
+	if (splitValue != "") {
+		result.push(splitValue);
+	}
+	return result;
+};
+
+// split input value by "+"
+const additionCalculator = function (value) {
+  const initialValue=0;
+  const valueArray = split(value, '+');
+  const valueNumber = valueArray.map(x => substractionCalculator(x));
+  const result = valueNumber.reduce((x,y) => x+y, initialValue);
+  console.log(result);
+  return result;
 }
 
-const calculator = function (value) {
-  console.log(value);
+// split input value by "*"
+const multiplicationCalculator = function (value) {
+  const initialValue=1;
+  const valueArray = split(value, '*');
+  const valueNumber = valueArray.map(x => {
+    if (x[0] == '(') {
+      const expression = x.substr(1, x.length-2);
+      return additionCalculator(expression);
+    }
+    return +x;
+  });
+  const result = valueNumber.reduce((x,y) => x*y, initialValue)
+  return result;
 }
+
+// split input value by "/"
+const divisionCalculator = function (value) {
+  const initialValue=1;
+  const valueArray = split(value, '/');
+  const valueNumber = valueArray.map(x => {
+    if (x[0] == '(') {
+      const newValue = x.substr(1, x.length-2);
+      return additionCalculator(newValue);
+    }
+    return +x;
+  });
+  const result = valueNumber.reduce((x,y) => x/y, initialValue)
+  return result;
+}
+
+// split input value by "-"
+const substractionCalculator = function (value) {
+  const valueArray= split(value, '-');
+  const valueNumber=valueArray.map(x => multiplicationCalculator(x));
+  const initialValue = valueNumber[0];
+  const result = valueNumber.slice(1).reduce((x,y) => x-y, initialValue);
+  return result;
+}
+
 document.addEventListener('DOMContentLoaded', init);
